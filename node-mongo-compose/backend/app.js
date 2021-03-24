81161b9612ff1773ecba832e913ced48cceb58b6
Paulo Ricardo <p.ricardodev@gmail.com>
2021-03-24 20:26:39 -0300

    Atualizacao exemplo node-mongo-compose

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const restful = require('node-restful');
 const server = express();
 const mongoose = restful.mongoose;
-const bodyParse = require('body-parser'); // Parse do conteúdo que vier na requisição do request
-const cors = require('cors');
 
 // Database
 
@@ -13,24 +11,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://db/mydb');
 
 // Teste print
-//server.get('/', (req, res, next) => res.send('<h1>Backend</h1>'));
-
-// Middlewares
-server.use(bodyParse.urlencoded({extended:true})); // Para envio de formulário
-server.use(bodyParse.json()); // Formato do conteúdo
-server.use(cors());
-
-// mapeamento objeto documento - ODM
-const Client = restful.model('Client', {
-    name: { type: String, required: true}
-});
-
-// Rest API
-Client.methods(['get', 'post', 'update', 'delete']);
-Client.updateOptions({new: true, runValidators: true});
-
-// Routes - Pega os métodos e cria as rotas especificas baseando-se nos methods
-Client.register(server, '/clients');
+server.get('/', (req, res, next) => res.send('<h1>Backend</h1>'));
 
 // Start server
 server.listen(3000);
