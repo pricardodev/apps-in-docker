fb05e850f4161185f972551acf3a118eb34e2983
Paulo Ricardo Gomes <ricardo_albuquerque@uvanet.com>
2021-03-26 11:22:30 -0300

    Criando proxy reverso

---
+++ email-worker-compose/app/app.sh
@@ -0,0 +1,2 @@
+pip install bottle==0.12.13
+python -u sender.py
--- email-worker-compose/scripts/init.sql
+++ email-worker-compose/scripts/init.sql
@@ -3,9 +3,9 @@ create database email_sender;
 /*Criando a conexão com banco criado*/
 \c email_sender
 
-create table emails {
+create table emails (
     id serial not null,
     date timestamp not null default current_timestamp,
     assunto varchar(100) not null,
     mensagem varchar(250) not null
-}
+);
